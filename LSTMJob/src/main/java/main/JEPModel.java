9a5095927e7013d1e25cb90ca06bbfc32a3886ed
JonathanArns <jonathan.arns@googlemail.com>
2019-11-18 10:04:31 +0100

    resolve merge conflict

--- LSTMJob/src/main/java/main/JEPModel.java
+++ LSTMJob/src/main/java/main/JEPModel.java
@@ -5,14 +5,18 @@ import jep.*;
 public class JEPModel {
     public static void main(String[] args) {
         try(Interpreter interpreter = new SharedInterpreter()) {
+            Double[] data = {};
+            NDArray<Double[]> input = new NDArray<>(data, 1, 11, 6);
+
             interpreter.exec("import numpy as np");
             interpreter.exec("import keras");
             interpreter.exec("from sklearn.externals import joblib");
             interpreter.exec("scaler = joblib.load('src/main/resources/scaler_lstm.save')");
-            interpreter.exec("features = scaler.transform(x)");
             interpreter.exec("model = keras.models.load_model('src/main/resources/my_model.h5')");
-            interpreter.exec("x = np.array([[[1]]])");
-            Object result = interpreter.getValue("model.predict(x)");
+
+            interpreter.set("input", input);
+            interpreter.exec("features = scaler.transform(input)");
+            Object result = interpreter.getValue("model.predict(features)");
             System.out.println(result);
             //TODO remove dl4j stuff
             //TODO minmax
