b384b11610fd7ab2dcf26af2a9d78df9c8a6ee99
KonstantinGasser <1810507@stud.hs-mannheim.de>
2019-11-26 10:52:08 +0100

    LSTM model in init kpy

--- infrastructure/docker-images/ray-master/kafka_py.py
+++ infrastructure/docker-images/ray-master/kafka_py.py
@@ -1,10 +1,10 @@
 import sys, os
 sys.path.append('/usr/src/app/')
 from distributed_system import distributed
+from distributed_system.lstm.model_creation import LSTM
 
 
-
-def run(input_topic, output_topic, bootstrap_server):
+def run(input_topic, output_topic, bootstrap_server, lstm_model):
     """
     this function will be called as soon as the docker container has started.
     It will call the init() function of ray in order to build a cluster for balancing the workers and actors on the distrebuted system.
@@ -19,11 +19,13 @@ def run(input_topic, output_topic, bootstrap_server):
 if __name__ == '__main__':
     arguments = sys.argv
     if not arguments: raise Exception
-
+    
+    lstm_model = LSTM()
     run(
         input_topic=arguments[1],
         output_topic=arguments[2],
-        bootstrap_server=arguments[3]
+        bootstrap_server=arguments[3],
+        lstm_model=lstm_model
         )
         
     
